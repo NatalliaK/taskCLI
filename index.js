ad075b7a90237af1ac3a86c8e02498ce01e2ddaa
NatalliaK <studina-natali@yandex.by>
2019-04-06 15:24:18 +0300

    Add more modules

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const program = require('commander');
 const request = require('./lib/request');
+const printToConsole = require('./lib/print');
 const custEnv = require('custom-env').env(true);
 
 program
@@ -37,19 +38,3 @@ program.on('--help', () => {
 });
 
 program.parse(process.argv);
-
-function printToConsole(weather) {
-  const {
-    city,
-    country,
-    temp,
-    wind,
-    overcast
-  } = weather;
-  console.log('');
-  console.log(`city: ${city}`);
-  console.log(`country: ${country}`);
-  console.log(`temperature: ${temp} degrees Celsius`);
-  console.log(`wind: ${wind}mps`);
-  console.log(`overcast: ${overcast}`);
-}
